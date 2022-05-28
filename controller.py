932d0d85a2e717f1ef970039ffa674e008b9d22e
RogerXiao0718 <deo2468977@gmail.com>
2022-05-28 19:48:44 +0800

    feature: load file and directory features

--- controller.py
+++ controller.py
@@ -4,6 +4,7 @@ from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QFileDialog, QMessageBox
 from PyQt5.QtCore import QStringListModel
 from UI import Ui_MainWindow
+import numpy as np
 import random
 import os
 import cv2
@@ -34,7 +35,7 @@ class MainWindow_controller(QtWidgets.QMainWindow):
 
     def loadFilesClicked(self):
         loaded_filepaths, filetype = QFileDialog.getOpenFileNames(self, "Open Images", "./",
-                                                                  filter="Images (*.png *.jpg *.jiff *.bmp)")
+                                                                  filter="Images (*.png *.jpg *.jfif *.bmp)")
         if loaded_filepaths:
             self.data["loaded_filepaths"] = loaded_filepaths
             self.data["loaded_filenames"] = list(map(lambda filepath: os.path.basename(filepath), loaded_filepaths))
@@ -42,7 +43,7 @@ class MainWindow_controller(QtWidgets.QMainWindow):
 
     def loadFolderClicked(self):
         loaded_folder = QFileDialog.getExistingDirectory(self, "Open Directory", "./")
-        extension_whitelist = [".png", ".jpg", ".jiff", ".bmp"]
+        extension_whitelist = [".png", ".jpg", ".jfif", ".bmp"]
         if loaded_folder:
             loaded_filenames = os.listdir(loaded_folder)
             loaded_filenames = list(filter(lambda filename: os.path.splitext(filename)[1] in extension_whitelist, loaded_filenames))
@@ -56,5 +57,41 @@ class MainWindow_controller(QtWidgets.QMainWindow):
         self.ui.filename_listwidget.addItems(
             [f"[{i}] {filename}" for i, filename in enumerate(loaded_filenames)])
 
+    def imageDisplay(self, image):
+        self.resize_displayed_images(image)
+
+        if len(self.displayed_image.shape) == 3:
+            height, width, channel = self.displayed_image.shape
+            bytesPerLine = width * channel
+            self.qimage = QImage(self.displayed_image, width, height, bytesPerLine, QImage.Format_BGR888)
+            self.ui.imageDisplayer.setPixmap(QPixmap.fromImage(self.qimage))
+        elif len(self.displayed_image.shape) == 2:
+            height, width = self.displayed_image.shape
+            bytesPerLine = width * 1
+            self.qimage = QImage(self.displayed_image, width, height, bytesPerLine, QImage.Format_Grayscale8)
+            self.ui.imageDisplayer.setPixmap(QPixmap.fromImage(self.qimage))
+
+    def resize_displayed_images(self, image):
+        displayer_size = self.ui.imageDisplayer.size()
+        image_height = image.shape[0]
+        image_width = image.shape[1]
+        if image_height > displayer_size.height() and image_height >= image_width:
+            image_hw_ratio = image_height / image_width
+            new_image_height = displayer_size.height()
+            new_image_width = int(new_image_height / image_hw_ratio)
+            self.displayed_image = cv2.resize(image, (new_image_width, new_image_height),
+                                              interpolation=cv2.INTER_AREA)
+        elif image_width > displayer_size.width() and image_width >= image_height:
+            image_wh_ratio = image_width / image_height
+            new_image_width = displayer_size.width()
+            new_image_height = int(new_image_width / image_wh_ratio)
+            self.displayed_image = cv2.resize(image, (new_image_width, new_image_height),
+                                              interpolation=cv2.INTER_AREA)
+        else:
+            self.displayed_image = image
+
     def listViewClicked(self, qmodelIndex):
-        QMessageBox.information(self, "Prompt", f"You selected {self.data['loaded_filenames'][qmodelIndex.row()]}")
+        self.cv2_image = cv2.imdecode(np.fromfile(self.data["loaded_filepaths"][qmodelIndex.row()], dtype=np.uint8), -1)
+        self.imageDisplay(self.cv2_image)
+
+
